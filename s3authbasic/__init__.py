bdef672941aacec1a2acc3ebb8745290c906cf22
ant30 <ant30tx@gmail.com>
2014-04-12 18:48:56 +0200

    Implemented the authbasic for the pyramid template

--- s3authbasic/__init__.py
+++ s3authbasic/__init__.py
@@ -1,10 +1,73 @@
+import os
+from hashlib import sha256
+
+from pyramid.authentication import BasicAuthAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.config import Configurator
+from pyramid.security import Allow, Authenticated
+
+
+def check_credentials(username, password, request):
+    credentials = request.registry.settings['credentials']
+    passwordhash = credentials.get(username, None)
+
+    passwordhashed = sha256(password).hexdigest()
+    if passwordhash is not None and passwordhashed == passwordhash:
+        return [username]
+
+
+def read_settings(settings, key, default=None):
+    env_key = key.upper()
+    if env_key in os.environ:
+        return os.environ.get(env_key)
+    else:
+        return settings.get(key, default)
+
+
+def read_users(settings):
+    credentials = {}
+    key_prefix = 'user_'
+
+    for key in settings.keys():
+        if key.startswith(key_prefix):
+            username = key.replace(key_prefix, '')
+            password = settings.get(key)
+            credentials[username] = password
+
+    for key in os.environ.keys():
+        if key.startswith(key_prefix):
+            username = key.replace(key_prefix, '')
+            password = os.environ.get(key)
+            credentials[username] = password
+
+    return credentials
+
+
+class RootSite(object):
+    __acl__ = [
+        (Allow, Authenticated, 'view'),
+    ]
+
+    def __init__(self, request):
+        pass
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+    settings['credentials'] = read_users(settings)
+    settings['realm'] = read_settings(settings, 'realm',
+                                      default='Protected site')
+
+    authn_policy = BasicAuthAuthenticationPolicy(check_credentials,
+                                                 realm=settings['realm'],
+                                                 debug=True)
+    authz_policy = ACLAuthorizationPolicy()
+
+    config = Configurator(settings=settings,
+                          root_factory=RootSite)
+    config.set_authentication_policy(authn_policy)
+    config.set_authorization_policy(authz_policy)
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
--- s3authbasic/views.py
+++ s3authbasic/views.py
@@ -1,6 +1,16 @@
-from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPUnauthorized
+from pyramid.security import forget
+from pyramid.view import view_config, forbidden_view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@forbidden_view_config()
+def basic_challenge(request):
+    response = HTTPUnauthorized()
+    response.headers.update(forget(request))
+    return response
+
+
+@view_config(route_name='home', renderer='templates/mytemplate.pt',
+             permission='view')
 def my_view(request):
     return {'project': 's3authbasic'}
